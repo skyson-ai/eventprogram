b5fc1b428114afdd70dc595d34b509e0debd9849
Ano2225 <ouatt0767@gmail.com>
2024-09-17 14:55:13 +0000

    Fix Assets Public Bug

--- lancement-event-1000f/src/app/page.tsx
+++ lancement-event-1000f/src/app/page.tsx
@@ -1,8 +1,10 @@
+import HeaderSection from "./components/HeaderSection";
 import IntroSection from "./components/IntroSection";
 
 export default function Home() {
   return (
     <>
+      <HeaderSection/>
       <IntroSection/>
     </>
   );
