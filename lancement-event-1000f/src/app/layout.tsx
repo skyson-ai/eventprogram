87a242ed1c388ac8fab8a5612c9d293240223567
Ano2225 <ouatt0767@gmail.com>
2024-09-18 23:24:54 +0000

    Update MetaData in layout.tsx

--- lancement-event-1000f/src/app/layout.tsx
+++ lancement-event-1000f/src/app/layout.tsx
@@ -7,9 +7,10 @@ import Header from "./components/Header";
 const inter = DM_Sans({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
 export const metadata: Metadata = {
-  title: "Lancement de 1000 Femmes Proprietaires",
-  description: "1000 Femmes Proprietaires est un programme visant à encourager les femmes de la diaspora à investir dans l'immobilier quel que soit leurs situations et statuts, pour favoriser l'autonomie financière et la constitution d'un patrimoine",
+  title: "Mireille Lebouler - Conseillère en strategie d'investissement immobilière",
+  description: "Découvrez le programme 1000 Femmes Propriétaires dirigé par l'experte Mireille Lebouler. Ce programme vise à encourager les femmes de la diaspora à investir dans l'immobilier pour favoriser l'autonomie financière et la constitution d'un patrimoine, quel que soit leur statut ou situation.",
 };
+
 
 export default function RootLayout({
   children,
