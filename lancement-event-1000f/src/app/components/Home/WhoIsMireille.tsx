31ce434ebbe8f07a2ee3a10aec929477d23debc8
skyson-ai <123488406+skyson-ai@users.noreply.github.com>
2024-09-19 11:10:35 +0000

    Update WhoIsMireille.tsx

--- lancement-event-1000f/src/app/components/Home/WhoIsMireille.tsx
+++ lancement-event-1000f/src/app/components/Home/WhoIsMireille.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 
 const WhoisMireille: React.FC = () => {
     return (
-        <section className="bg-white p-4 md:p-16">
+        <section className="bg-white p-0 md:p-16">
             {/* Image pleine largeur */}
-            <div className="relative w-full h-64 md:h-[600px]">
+            <div className="relative w-full h-96 md:h-[600px]">
                 <Image
                     src="/Mireille.jpg" // Chemin vers la photo de Mireille
                     alt="Mireille Le Bouler"
@@ -17,12 +17,12 @@ const WhoisMireille: React.FC = () => {
             </div>
 
             {/* Texte en dessous avec contour stylisé */}
-            <div className="relative z-10 -mt-12 md:-mt-16 mx-auto max-w-4xl p-6 md:p-12 bg-white border-4 border-yellow-500 rounded-lg shadow-lg">
-                <h2 className="text-2xl md:text-4xl font-bold text-gray-800 text-center">Qui est Mireille Le Bouler ?</h2>
-                <p className="mt-4 text-base md:text-lg text-gray-600 leading-relaxed text-center">
+            <div className="relative z-10 -mt-8 md:-mt-12 mx-auto max-w-4xl p-6 md:p-12 bg-white border-4 border-yellow-500 rounded-lg shadow-lg">
+                <h2 className="text-3xl md:text-4xl font-bold text-gray-800 text-center">Qui est Mireille Le Bouler ?</h2>
+                <p className="mt-4 text-lg text-gray-600 leading-relaxed text-center">
                     Mireille Le Bouler est une experte en immobilier qui a réalisé son premier achat immobilier à l&apos;âge de 22 ans. Elle dirige Uriel Immobilier, une société qui accompagne les individus dans leurs projets immobiliers.
                 </p>
-                <p className="mt-4 text-base md:text-lg text-gray-600 leading-relaxed text-center">
+                <p className="mt-4 text-lg text-gray-600 leading-relaxed text-center">
                     Connue pour sa passion et son expertise, elle s&apos;est imposée comme une figure de proue en aidant les gens, notamment les femmes, à atteindre l&apos;indépendance financière grâce à l&apos;investissement immobilier. Son approche unique et ses résultats concrets font d&apos;elle une leader dans son domaine.
                 </p>
             </div>
